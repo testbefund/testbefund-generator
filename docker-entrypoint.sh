3bd2d9f5e4d585a5196ee0dee6ab384e17923923
nt <nt@hbt.de>
2020-06-28 13:39:15 +0200

    Fix'd Docker image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "const TESTBEFUND_API_URL = '$TESTBEFUND_API_URL'; const CLIENT_ID = '$CLIENT_ID'; const AZURE_AD_AUTHORITY = '$AZURE_AD_AUTHORITY';" > /usr/share/nginx/html/assets/config.js
+echo "const TESTBEFUND_PATIENT_URL = '$TESTBEFUND_PATIENT_URL'; const TESTBEFUND_API_URL = '$TESTBEFUND_API_URL'; const CLIENT_ID = '$CLIENT_ID'; const AZURE_AD_AUTHORITY = '$AZURE_AD_AUTHORITY';" > /usr/share/nginx/html/assets/config.js
 nginx -g "daemon off;"
